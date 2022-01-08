d3083aba7d158a66be3adaf669292c37409976ef
SinanGentoo <sinangentoo@gmail.com>
2022-01-08 18:04:37 +0800

    Rewrite

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Extension, type, api } = require('clipcc-extension');
+const userscript = require('./userscript.js');
+
+class Loader extends Extension {
+    async onInit() {
+        const vm = api.getVmInstance();
+        const block = api.getBlockInstance();
+        try {
+            await userscript.default(vm, block);
+        } catch (e) {
+            alert("Load Error:"+e);
+        }
+    }
+}
+
+module.exports = Loader;
